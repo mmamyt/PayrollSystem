80ed07bc2c0b0cbe707a3c415f0c848b7718dc4e
mmamyt2 <mmamyt2@uic.edu>
2022-04-14 16:35:33 -0600

    testing

--- src/payroll/DB.java
+++ src/payroll/DB.java
@@ -6,10 +6,19 @@ import java.sql.SQLException;
 
 public class DB {
 
-    public static Connection getConnection() throws SQLException {
+/*    public static Connection getConnection() throws SQLException {
         String dbUrl = "jdbc:sqlite:C:\\SQLiteStudio\\PayrollSystem";
         Connection connection = DriverManager.getConnection(dbUrl);
         return connection;
     }
+    public static void main(String[] args) {
+        JDBCL_Lecture connect1 = new JDBCL_Lecture();
+        try {
+            connect1.getConnection();
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }*/
+
 }
 
---
+++ src/payroll/Test.java
@@ -0,0 +1,7 @@
+package payroll;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
